add25029c26a1712ff75048bfe008d8bce144faf
cosmicrisp <chi56151@gmail.com>
2022-11-08 14:51:58 -0500

    Code with the new test thing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Redwithredcorner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Redwithredcorner.java
@@ -10,28 +10,29 @@ import org.firstinspires.ftc.teamcode.util.Logger;
 @Autonomous
 public class Redwithredcorner extends LinearOpMode {
     private final Logger logger = new Logger(telemetry, true);
-
+    enum SleeveImage {ONE, TWO, THREE}
     @Override
     public void runOpMode() throws InterruptedException {
         TestRobot robot = new TestRobot(hardwareMap, logger);
         robot.initHardware();
-//117 cm of working space or 46 inches
+//140 cm of working space
         Drive2 drive = new Drive2(robot, this);
 
         waitForStart();
 
         //state the case when testing
-        logger.info("a");
-        String test = "none";
+
+        SleeveImage test = SleeveImage.ONE;
+        logger.info("sleeveresults " + test);
         switch(test) {
-            case "one":
+            case ONE:
                 drive.navigationMonitorTicks(100, -100, 0, 10);
                 drive.navigationMonitorTicks(100, 0, 145, 10);
                 break;
-            case "two":
+            case TWO:
                 drive.navigationMonitorTicks(100, 0, 145, 10);
                 break;
-            case "three":
+            case THREE:
                 drive.navigationMonitorTicks(100, 100, 0, 10);
                 drive.navigationMonitorTicks(100, 0, 145, 10);
                 break;
