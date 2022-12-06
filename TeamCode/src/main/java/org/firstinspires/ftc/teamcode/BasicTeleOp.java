861e699cba3f0c9f1ab3fa04e5c0cec9560577e0
JJTech0130 <jjtech@jjtech.dev>
2022-12-06 17:38:37 -0500

    move changes from ArmTest.java to BasicTeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
@@ -5,11 +5,19 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.robot.PowerPlayBotV2;
 import org.firstinspires.ftc.teamcode.robot.Robot;
+import org.firstinspires.ftc.teamcode.robot.hardware.Arm;
+import org.firstinspires.ftc.teamcode.util.Debouncer;
 import org.firstinspires.ftc.teamcode.util.Logger;
+import org.firstinspires.ftc.teamcode.util.Toggle;
 
 @TeleOp(name = "TeleOp", group = "TeleOp")
 public class BasicTeleOp extends LinearOpMode {
     Logger logger = new Logger(telemetry);
+
+    Toggle manualOverride = new Toggle();
+    Toggle liftHold = new Toggle();
+    int liftHoldValue = -500;
+    Debouncer aButton = new Debouncer();
 
     @Override
     public void runOpMode() {
@@ -70,19 +78,42 @@ public class BasicTeleOp extends LinearOpMode {
             // END DRIVE
 
             // BEGIN ARM
-            // Begin Lift
-            if (gamepad2.right_trigger != 0) {
-                robot.arm.lift.setPower(gamepad2.right_trigger);
-            } else if (gamepad2.left_trigger != 0) {
-                robot.arm.lift.setPower(-gamepad2.left_trigger);
-            }
-            // End Lift
+            if (manualOverride.update(gamepad2.x)) {
+                // Begin Lift
+                telemetry.addData("Lift", robot.arm.lift.getCurrentPosition());
+                if (liftHold.update(gamepad2.y)) {
+                    if (liftHoldValue != -500) {
+                        robot.arm.lift.setTargetPosition(liftHoldValue);
+                    }
+                } else {
+                    liftHoldValue = robot.arm.lift.getCurrentPosition();
+                    robot.arm.lift.setPower(gamepad2.right_trigger - gamepad2.left_trigger);
+                }
+                // End Lift
 
-            // Begin Reacher
-            if (gamepad2.right_stick_x != 0) {
+                // Begin Reacher
+                telemetry.addData("Reach", robot.arm.reacher.getCurrentPosition());
                 robot.arm.reacher.setPower(gamepad2.right_stick_x);
+                // End Reacher
+            } else {
+                // Begin Lift
+                telemetry.addData("Lift", robot.arm.lift.getCurrentPosition());
+                if (gamepad1.right_bumper) {
+                    robot.arm.lift.setPreset(Arm.Lift.Preset.DRIVING);
+                } else if (aButton.update(gamepad1.a)) {
+                    robot.arm.pincher.contract();
+                    robot.arm.lift.setPreset(Arm.Lift.Preset.GRAB);
+                    robot.arm.pincher.expand();
+                } else if (gamepad1.x) {
+                    robot.arm.lift.setPreset(Arm.Lift.Preset.LOW_POLE);
+                } else if (gamepad1.y) {
+                    robot.arm.lift.setPreset(Arm.Lift.Preset.MEDIUM_POLE);
+                } else if (gamepad1.b) {
+                    robot.arm.lift.setPreset(Arm.Lift.Preset.HIGH_POLE);
+                } else if (gamepad1.left_bumper) {
+                    robot.arm.pincher.contract();
+                }
             }
-            // End Reacher
 
             // Begin Pincher
             if (gamepad2.left_bumper) {
@@ -91,8 +122,6 @@ public class BasicTeleOp extends LinearOpMode {
                 robot.arm.pincher.contract();
             }
             // End Pincher
-
-
             // END ARM
 
             telemetry.update(); // Update telemetry
