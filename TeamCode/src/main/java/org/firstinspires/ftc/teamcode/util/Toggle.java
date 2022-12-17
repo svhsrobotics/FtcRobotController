d21dd4e66098cf7331783ef0bbf0ad195a13a433
JJTech0130 <jjtech@jjtech.dev>
2022-12-17 12:54:20 -0500

    optimize toggle code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Toggle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Toggle.java
@@ -10,20 +10,14 @@ public class Toggle {
     private boolean lastState = false;
 
     public boolean update(boolean buttonState) {
-        // If the button is pressed
-        if (buttonState) {
-            // If the button's last state was not pressed
-            if (!lastState) {
-                // Toggle our current state
-                state = !state;
-            }
-            // The button was last pressed
-            lastState = true;
-        // The button is not pressed
-        } else {
-            // The button was last not pressed
-            lastState = false;
+        // If the button is pressed and our last state was not pressed
+        if (buttonState && !lastState) {
+            // Toggle our current state
+            state = !state;
         }
+
+        // Save the state
+        lastState = buttonState;
 
         return state;
     }
