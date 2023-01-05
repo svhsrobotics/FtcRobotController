cf1bcb008698225de2ad9f67b18ae9b8cf5ff7f5
cosmicrisp <chi56151@gmail.com>
2023-01-05 14:35:02 -0500

    commiting before merging master

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AllAutomovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AllAutomovement.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.Shared.Drive2;
 import org.firstinspires.ftc.teamcode.robot.PowerPlayBotV2;
+import org.firstinspires.ftc.teamcode.robot.hardware.Arm;
 import org.firstinspires.ftc.teamcode.util.Logger;
 //import org.firstinspires.ftc.teamcode.vision.TensorflowStandardSleeve;
 import org.firstinspires.ftc.teamcode.vision.TensorflowStandardSleeve;
@@ -29,23 +30,36 @@ public class AllAutomovement extends LinearOpMode {
         TfodSleeve detected = tensor.scanStandardSleeve();
 
         logger.info("Sleeve Detected: " + detected);
-
+        // Get the cone in the pincher and get ready to drive
+        robot.arm.pincher.expand();
         // Move up to the pole
         drive.navigationMonitorTicks(12,0,3.5,10);
+        robot.arm.lift.setPreset(Arm.Lift.Preset.DRIVING);
         drive.ceaseMotion();
-        drive.navigationMonitorTicks(15,-32 - 10,0,10);
+        drive.navigationMonitorTicks(15,32 + 30,0,10);
         drive.ceaseMotion();
         drive.navigationMonitorTicks(25, 0, 66.5, 10);
         drive.ceaseMotion();
         drive.navigationMonitorTicks(13, 0, 18, 10);
         drive.ceaseMotion();
-        drive.navigationMonitorTicks(20, 32, 0, 10);
+        drive.navigationMonitorTicks(20, -25, 0, 10);
         drive.ceaseMotion();
+        //drive.na
+        drive.navigationMonitorTicksPhi(0, 10, 0 ,70,1);
 
+        robot.arm.lift.setPreset(Arm.Lift.Preset.HIGH_POLE);
+        while (robot.arm.lift.isBusy() && !isStopRequested()) {}
+        robot.arm.reacher.setTargetPosition(2000);
+        while (robot.arm.reacher.isBusy() && !isStopRequested()) {}
+        robot.arm.pincher.contract();
+        robot.arm.reacher.setTargetPosition(0);
+        while (robot.arm.reacher.isBusy() && !isStopRequested()) {}
+        robot.arm.lift.setPreset(Arm.Lift.Preset.DRIVING);
+        while (robot.arm.lift.isBusy() && !isStopRequested()) {}
         // This is where the pole would be dropped
 
 
-        switch(detected) {
+        /*switch(detected) {
             case THREE: // This is the rightmost slot
                 drive.navigationMonitorTicks(20, 70.5, 0, 10);
                 drive.ceaseMotion();
@@ -58,7 +72,7 @@ public class AllAutomovement extends LinearOpMode {
                 drive.navigationMonitorTicks(20, -32, 0, 10);
                 drive.ceaseMotion();
                 break;
-        }
+        }*/
     }
 }
 
