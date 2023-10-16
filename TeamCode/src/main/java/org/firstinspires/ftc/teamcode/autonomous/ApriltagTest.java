8df07536c2c90a65a0c0ee156e171c60599d1478
JJTech0130 <jjtech@jjtech.dev>
2023-10-16 16:15:06 -0400

    trying to get apriltag logging working

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/ApriltagTest.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+
+@Autonomous(name = "AprilTag Test")
+public class ApriltagTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        AprilTagProcessor aprilTag = new AprilTagProcessor.Builder().build();
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+        builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+        builder.addProcessor(aprilTag);
+        VisionPortal visionPortal = builder.build();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+            telemetry.addData("# AprilTags Detected", currentDetections.size());
+
+            for (AprilTagDetection detection : currentDetections) {
+                if (detection.metadata != null) {
+                    //telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                    telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+
+                    telemetry.addLine(String.format("Field XYZ %6.1f %6.1f %6.1f  (inch)", detection.metadata.fieldPosition.get(0), detection.metadata.fieldPosition.get(1), detection.metadata.fieldPosition.get(2)));
+                }
+            }
+        }
+
+        visionPortal.close();
+
+    }
+}
