cbfee903b83e8cb05095cc8553797227d06f2557
JJTech0130 <jjtech@jjtech.dev>
2023-12-15 09:34:20 -0500

    panthera more stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TeleOp.java
@@ -1,47 +1,45 @@
 package org.firstinspires.ftc.teamcode.opmode;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-import org.firstinspires.ftc.teamcode.drive.Robot;
 import org.firstinspires.ftc.teamcode.drive.panthera.PantheraDrive;
-import org.firstinspires.ftc.teamcode.drive.psi.PsiDrive;
 import org.firstinspires.ftc.teamcode.util.GlobalOpMode;
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp
+@Config
 public class TeleOp extends LinearOpMode {
     DcMotorEx barHangMotor;
     DcMotorEx flipperMotor;
+    DcMotorEx intakeMotor;
+
+    double ispeed = 0;
+
+    public static double FLIPPER_POWER = 0.6;
+    public static int FLIPPER_POS = -475;
 
     @Override
     public void runOpMode() throws InterruptedException {
         GlobalOpMode.opMode = this;
-        // CHANGE THIS HERE TO CHANGE THE BOT TYPE
         PantheraDrive drive = new PantheraDrive(hardwareMap);
-        Robot robot = new Robot(hardwareMap);
         barHangMotor = hardwareMap.get(DcMotorEx.class, "hangMotor");
-        barHangMotor.setTargetPosition(barHangMotor.getCurrentPosition());
-        barHangMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        if (robot.botType != Robot.BotType.ROBOTICA) {
-            telemetry.addData("Error", "This opmode is only compatible with the PSI bot");
-            telemetry.update();
-            return;
-        }
-
-//        AprilTagCamera[] cameras = new AprilTagCamera[3];
-//        cameras[0] = new AprilTagCamera(hardwareMap.get(WebcamName.class, "Left"), 8, Math.toRadians(70), Math.toRadians(-45));
-//        cameras[1] = new AprilTagCamera(hardwareMap.get(WebcamName.class, "Center"), 7, Math.toRadians(90), Math.toRadians(0));
-//        cameras[2] = new AprilTagCamera(hardwareMap.get(WebcamName.class, "Right"), 8, Math.toRadians(-70), Math.toRadians(45));
-
+        intakeMotor = hardwareMap.get(DcMotorEx.class, "pdw_intake");
         flipperMotor = hardwareMap.get( DcMotorEx.class,"flipper");
+
+        barHangMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        flipperMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        intakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        barHangMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
         flipperMotor.setTargetPosition(flipperMotor.getCurrentPosition());
         flipperMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         waitForStart();
-
 
         while (!isStopRequested()) {
             drive.update(); // MUST be called every loop cycle so that RoadRunner calculates the pose correctly
@@ -60,74 +58,47 @@ public class TeleOp extends LinearOpMode {
                 drive.setPoseEstimate(new Pose2d(poseEstimate.getX(), poseEstimate.getY(), 0));
             }
             // TODO: Add controls to gamepad 2 as well
-            if (gamepad1.a) {
-                // Intake running while A is held
-
-            } else {
-                // Turn off intake when A released
-            }
 
             if (gamepad1.b) {
-                // Move pixel hand servo to release 1 pixel
-                // TODO: Do we need to debounce and count presses? Svit mentioned a "pixel cassette" that drops multiple pixels
                 throwPixel();
-            } else {
-                // Close pixel hand servo
-                //resetFlipper();
-            }
-            if (gamepad1.a) {
+            } else if (gamepad1.a) {
                 resetFlipper();
             }
-            if (gamepad1.right_bumper && gamepad1.left_bumper && getRuntime() >= 85) { // technically endgame is 90sec, we let them launch a little early just in case
-                // Launch the airplane
-            }
 
-            // Set bar hang motor power to gamepad1.right_trigger - gamepad1.left_trigger for analog control
-
-//            if (gamepad1.right_trigger >.1 && barHangMotor.getTargetPosition() < 538 ) {
-//
+//            if (gamepad1.right_bumper && gamepad1.left_bumper && getRuntime() >= 85) { // technically endgame is 90sec, we let them launch a little early just in case
+//                // Launch the airplane
 //            }
-            if (gamepad1.y) {
-                barHang();
+
+            if (gamepad1.right_trigger > 0.1 && barHangMotor.getCurrentPosition() > -8490) {
+                barHangMotor.setPower(-gamepad1.right_trigger * 0.5);
+            } else if (gamepad1.left_trigger > 0.1 && barHangMotor.getCurrentPosition() < -50) {
+                barHangMotor.setPower(gamepad1.left_trigger * 0.5);
+            } else {
+                barHangMotor.setPower(0);
             }
 
+            if (gamepad1.dpad_up) {
+                ispeed = 0.6;
+            } else {
+                ispeed = 0;
+            }
+
+            intakeMotor.setPower(ispeed);
+            telemetry.addData("ispeed", ispeed);
+            telemetry.addData("bar hang", barHangMotor.getCurrentPosition());
+            telemetry.addData("flipper ticks", flipperMotor.getCurrentPosition());
+            telemetry.update();
         }
-
-
-
-
-
     }
-    void barHang() {
-
-        barHangMotor.setTargetPosition(538);
-        barHangMotor.setPower(0.2);
-
-
-    }
-    int startFlipperTicks=0;
 
     void throwPixel() {
-        int flipperTicks = flipperMotor.getCurrentPosition();
-        if (startFlipperTicks==0) {
-           startFlipperTicks=flipperTicks;
-        }
-        flipperMotor.setTargetPosition(flipperTicks-475);
-        flipperMotor.setPower(0.1);
-        //flipperMotor.
-        telemetry.addData("flipper ticks", flipperTicks);
-        telemetry.update();
-
+        flipperMotor.setTargetPosition(FLIPPER_POS);
+        flipperMotor.setPower(FLIPPER_POWER);
     }
+
     void resetFlipper() {
-        flipperMotor.setTargetPosition(startFlipperTicks);
+        flipperMotor.setTargetPosition(0);
     }
-    //alelluia deus omnipotens confitor deo omnipotenti
-
-    //in nomine patris, et fili, et spiritu sancti
-
-
-
 }
 
 
