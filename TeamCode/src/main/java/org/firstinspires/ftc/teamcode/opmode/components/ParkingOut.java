2de0268f3467485e0a2506354fefbf04cc530760
edmund.webb <edmund.webb@student.svpanthers.org>
2024-01-08 16:34:21 -0500

    fixing ryan's code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/components/ParkingOut.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/components/ParkingOut.java
@@ -6,6 +6,7 @@ import com.acmerobotics.roadrunner.geometry.Vector2d;
 import org.firstinspires.ftc.teamcode.drive.Robot;
 import org.firstinspires.ftc.teamcode.drive.TrajectoryDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequenceBuilder;
 
 public class ParkingOut extends Component{
 
@@ -14,34 +15,32 @@ public class ParkingOut extends Component{
     }
 
     @Override
-    public void drive() {getRobot().getDrive().getClass();}
-    public void parkOuter() {
+    public void drive() {
+
         //getRobot().getDrive().currentQuadrant() == TrajectoryDrive.Quadrant.BLUE_BOARD
 
         Pose2d currentPose = getRobot().getDrive().getPoseEstimate();
-        TrajectorySequence traj = null;
+        TrajectorySequenceBuilder trajB = getRobot().getDrive().trajectorySequenceBuilder(currentPose);
+
 
 
         if (getRobot().getDrive().currentQuadrant() == TrajectoryDrive.Quadrant.RED_AUDIENCE) {
-            traj = getRobot().getDrive().trajectorySequenceBuilder(currentPose)
-                    .lineTo(new Vector2d(currentPose.getX(), -(4*12+9)))
-                    .lineTo(new Vector2d(59, -57))
-                    .build();
+            trajB =trajB.lineTo(new Vector2d(currentPose.getX(), -(4*12+9)))
+                    .lineTo(new Vector2d(59, -57));
         } else if (getRobot().getDrive().currentQuadrant() == TrajectoryDrive.Quadrant.RED_BOARD) {
-            traj = getRobot().getDrive().trajectorySequenceBuilder(currentPose)
-                    .lineTo(new Vector2d(currentPose.getX(), -57))
-                    .lineTo(new Vector2d(59, -6))
-                    .build();
-        } else if (getRobot().getDrive().currentQuadrant() == TrajectoryDrive.Quadrant.BLUE_BOARD) {
-            traj = getRobot().getDrive().trajectorySequenceBuilder(currentPose)
-                    .lineTo(new Vector2d(currentPose.getX(), 57))
+            trajB = trajB.lineTo(new Vector2d(currentPose.getX(), -57))
+                    .lineTo(new Vector2d(59, -6));
 
-                    .lineTo(new Vector2d(59, 57))
-                    .build();
+        } else if (getRobot().getDrive().currentQuadrant() == TrajectoryDrive.Quadrant.BLUE_BOARD) {
+            trajB = trajB.lineTo(new Vector2d(currentPose.getX(), 57))
+                    .lineTo(new Vector2d(59, 57));
+
 
 
         }
+        TrajectorySequence traj = trajB.build();
+        getRobot().getDrive().followTrajectorySequence(traj);
     }
-
+//ryan i had to fix ur code it didn't copy ParkingIn correctly
 }
 
