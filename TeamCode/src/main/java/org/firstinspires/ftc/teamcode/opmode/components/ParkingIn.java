057ee35cd1a96b1cc38730a4fcd316189149d0e4
edmund.webb <edmund.webb@student.svpanthers.org>
2024-01-08 15:51:05 -0500

    continuing the compartment stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/components/ParkingIn.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/components/ParkingIn.java
@@ -19,38 +19,31 @@ public class ParkingIn extends Component{
 
     @Override
     public void drive() {
-        getRobot().getDrive().getClass();
-    }
-
-    public void parkInner() {
         //getRobot().getDrive().currentQuadrant() == TrajectoryDrive.Quadrant.BLUE_BOARD
 
         Pose2d currentPose = getRobot().getDrive().getPoseEstimate();
-        TrajectorySequence traj = null;
+        TrajectorySequenceBuilder trajB = getRobot().getDrive().trajectorySequenceBuilder(currentPose);
 
         if (getRobot().getDrive().currentQuadrant() == TrajectoryDrive.Quadrant.RED_AUDIENCE) {
-            traj = getRobot().getDrive().trajectorySequenceBuilder(currentPose)
-                    .lineTo(new Vector2d(currentPose.getX(), -6))
+            trajB = trajB.lineTo(new Vector2d(currentPose.getX(), -6))
                     .lineTo(new Vector2d(currentPose.getX(), -5 * 12 + 3))
                     .turn(Math.toRadians(90))
-                    .lineTo(new Vector2d(3 * 12 + 7, -6))
-                    .build();
+                    .lineTo(new Vector2d(3 * 12 + 7, -6));
         } else if (getRobot().getDrive().currentQuadrant() == TrajectoryDrive.Quadrant.RED_BOARD) {
-            traj = getRobot().getDrive().trajectorySequenceBuilder(currentPose)
-                    .lineTo(new Vector2d(currentPose.getX(), -6))
+            trajB = trajB.lineTo(new Vector2d(currentPose.getX(), -6))
                     .lineTo(new Vector2d(currentPose.getX(), -5 * 12 + 3))
                     .turn(Math.toRadians(90))
-                    .lineTo(new Vector2d(3 * 12 + 7, -6))
-                    .build();
+                    .lineTo(new Vector2d(3 * 12 + 7, -6));
         } else if (getRobot().getDrive().currentQuadrant() == TrajectoryDrive.Quadrant.BLUE_BOARD) {
-            traj = getRobot().getDrive().trajectorySequenceBuilder(currentPose)
-                    .lineTo(new Vector2d(currentPose.getX(), 6))
+            trajB = trajB.lineTo(new Vector2d(currentPose.getX(), 6))
                     .lineTo(new Vector2d(currentPose.getX(), 5 * 12 + 3))
                     .turn(Math.toRadians(90))
-                    .lineTo(new Vector2d(3 * 12 + 7, 6))
-                    .build();
+                    .lineTo(new Vector2d(3 * 12 + 7, 6));
 
         }
+
+        TrajectorySequence traj = trajB.build();
+
 
     }
 
