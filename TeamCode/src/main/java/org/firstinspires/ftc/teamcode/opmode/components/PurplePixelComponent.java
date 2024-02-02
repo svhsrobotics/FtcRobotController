68b6f0e10cc9e9f1fceb5d79f3bd8a52952bf052
JJTech0130 <jjtech@jjtech.dev>
2024-02-02 12:39:39 -0500

    clean that up a tad

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/components/PurplePixelComponent.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/components/PurplePixelComponent.java
@@ -1,43 +1,43 @@
 package org.firstinspires.ftc.teamcode.opmode.components;
 
-import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 
 import org.firstinspires.ftc.teamcode.drive.Robot;
-import org.firstinspires.ftc.teamcode.drive.TrajectoryDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequenceBuilder;
 import org.firstinspires.ftc.teamcode.vision.TensorFlowDetection;
 
 public class PurplePixelComponent extends Component {
-    Pose2d RED_AUDIENCE_TAPE_MARKS = new Pose2d(-3 * 12, -2 * 12 - 12);
-    Pose2d BLUE_AUDIENCE_TAPE_MARKS = new Pose2d(-3 * 12, 2 * 12 + 12);
-    Pose2d RED_BOARD_TAPE_MARKS = new Pose2d(12, -2 * 12 - 12);
-    Pose2d BLUE_BOARD_TAPE_MARKS = new Pose2d(12, 2 * 12 + 12);
+    Vector2d RED_AUDIENCE_TAPE_MARKS = new Vector2d(-3 * 12, -2 * 12 - 12);
+    Vector2d BLUE_AUDIENCE_TAPE_MARKS = new Vector2d(-3 * 12, 2 * 12 + 12);
+    Vector2d RED_BOARD_TAPE_MARKS = new Vector2d(12, -2 * 12 - 12);
+    Vector2d BLUE_BOARD_TAPE_MARKS = new Vector2d(12, 2 * 12 + 12);
     private final TensorFlowDetection.PropPosition propPosition;
     public PurplePixelComponent(Robot robot, TensorFlowDetection.PropPosition propPosition) {
         super(robot);
         this.propPosition = propPosition;
     }
 
-    private TrajectorySequenceBuilder goToTapeCenter(TrajectorySequenceBuilder trajB) {
-        if (getRobot().getDrive().currentQuadrant() == TrajectoryDrive.Quadrant.BLUE_BOARD) {
-            trajB = trajB.lineTo(new Vector2d(BLUE_BOARD_TAPE_MARKS.getX(), BLUE_BOARD_TAPE_MARKS.getY()));
-        } else if (getRobot().getDrive().currentQuadrant() == TrajectoryDrive.Quadrant.BLUE_AUDIENCE) {
-            trajB = trajB.lineTo(new Vector2d(BLUE_AUDIENCE_TAPE_MARKS.getX(), BLUE_AUDIENCE_TAPE_MARKS.getY()));
-        } else if (getRobot().getDrive().currentQuadrant() == TrajectoryDrive.Quadrant.RED_AUDIENCE) {
-            trajB = trajB.lineTo(new Vector2d(RED_AUDIENCE_TAPE_MARKS.getX(), RED_AUDIENCE_TAPE_MARKS.getY()));
-        } else if (getRobot().getDrive().currentQuadrant() == TrajectoryDrive.Quadrant.RED_BOARD) {
-            trajB = trajB.lineTo(new Vector2d(RED_BOARD_TAPE_MARKS.getX(), RED_BOARD_TAPE_MARKS.getY()));
+    private Vector2d currentTapeMarks() {
+        switch (getRobot().getDrive().currentQuadrant()) {
+            case BLUE_BOARD:
+                return BLUE_BOARD_TAPE_MARKS;
+            case BLUE_AUDIENCE:
+                return BLUE_AUDIENCE_TAPE_MARKS;
+            case RED_AUDIENCE:
+                return RED_AUDIENCE_TAPE_MARKS;
+            case RED_BOARD:
+                return RED_BOARD_TAPE_MARKS;
+            default:
+                return null;
         }
-        return trajB;
     }
 
     @Override
     public void drive() {
 
         TrajectorySequenceBuilder trajB = getRobot().getDrive().trajectorySequenceBuilder(getRobot().getDrive().getPoseEstimate());
-        trajB = goToTapeCenter(trajB);
+        trajB = trajB.lineTo(currentTapeMarks());
         int driveBackwards = 1;
 
 //        if (getRobot().getClass() == PsiBot.class) {
@@ -57,8 +57,8 @@ public class PurplePixelComponent extends Component {
         }
 
         trajB = trajB.addTemporalMarker(() -> getRobot().dropPurplePixel(true))
-                .waitSeconds(2);
-        trajB = goToTapeCenter(trajB);
+                .waitSeconds(2)
+                .lineTo(currentTapeMarks());
 
 
         TrajectorySequence traj = trajB.build();
