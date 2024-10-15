0549ac0598db1b1fe97ee07215ee042cae60857f
Nick Tomic 🤓 <nicholastt305@gmail.com>
2024-10-14 20:10:25 -0400

    learing - tictac.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tictacs.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.opmode;
+
+import android.graphics.Color;
+
+public class tictacs {
+    public String material = "plastic";
+    public int TicTac = 200;
+    public String  expiration = String.valueOf(2025);
+    public String Color = "red";
+    public tictacs TicTacs  () {
+        
+    }
+
+
+
+}
