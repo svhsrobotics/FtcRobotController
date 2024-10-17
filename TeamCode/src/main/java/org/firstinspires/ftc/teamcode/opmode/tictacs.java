4c5eb458827577e81f783f80b7e25d47c917f4c4
Nick Tomic 🤓 <nicholastt305@gmail.com>
2024-10-16 20:27:22 -0400

    nickteleop.java - robot drives and servos work.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tictacs.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tictacs.java
@@ -1,14 +1,13 @@
 package org.firstinspires.ftc.teamcode.opmode;
 
-import android.graphics.Color;
-
 public class tictacs {
     public String material = "plastic";
     public int TicTac = 200;
     public String  expiration = String.valueOf(2025);
     public String Color = "red";
     public tictacs TicTacs  () {
-        
+
+        return null;
     }
 
 
